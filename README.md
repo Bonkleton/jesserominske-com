79ee9b8718cd048f479d51507da2980215ecc996
Jesse Rominske <sidorak12814@gmail.com>
2017-04-28 01:36:06 +0000

    major cleanup

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 I'm hosting the source code for my personal website here!
 
 It's nowhere near finished as of yet, in case it's not obvious.
+
+The HTML is treated as a golang text template upon request, with the tags being
+filled in with the necessary resources.
+
+This way, the HTML and JavaScript can be organized into different files on the
+server, but the client, upon accessing the site, will see a single resource
+containing the entire front end. Furthermore, this also prevents there from
+being a situation where the front end has access to the server filesystem,
+since everything needed for the client is loaded at once.
+
+Current plan is to host a wiki for Terra Duenuo here as well. ;)
--- backend/server.go
+++ backend/server.go
@@ -7,38 +7,61 @@ import (
 	"text/template"
 )
 
-// HTTP request handler
-func handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/favicon.ico" { // load favicon.ico because who cares
-		p, err := ioutil.ReadFile("../favicon.ico")
+//list of keys and paths for resource loading
+var paths = map[string]string{
+	"Style":         "../css/css.css",
+	"Home":          "../view/home.html",
+	"Blog":          "../view/blog.html",
+	"About":         "../view/about.html",
+	"Connect":       "../view/connect.html",
+	"Wiki":          "../view/wiki.html",
+	"ContentScript": "../js/content.js"}
+
+//loads all relevant partials
+func loadPartials() (map[string]string, error) {
+	g := make(map[string]string)
+
+	//load resources from paths
+	for key, path := range paths {
+		body, err := ioutil.ReadFile(path)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return nil, err
+		}
+		g[key] = string(body)
+	}
+	return g, nil
+}
+
+//HTTP request handler
+func handler(wrt http.ResponseWriter, req *http.Request) {
+	if req.URL.Path == "/favicon.ico" { //load favicon.ico because who cares
+		f, err := ioutil.ReadFile("../favicon.ico")
+		if err != nil {
+			http.Error(wrt, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		fmt.Fprintf(w, "%s", p)
-	} else { // in all other cases load the index
-
-		// run grunt task to grab all partials
-		g, err := runGrunt()
+		fmt.Fprintf(wrt, "%s", f)
+	} else { //in all other cases load the index
+		//run grunt task to grab all partials
+		partials, err := loadPartials()
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(wrt, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
-		// get template function based on index and execute to load page
+		//get template function based on index and execute to load page
 		t, _ := template.ParseFiles("../index.html")
-		t.Execute(w, g)
+		t.Execute(wrt, partials)
 	}
 }
 
-// start the servers
+//start the servers
 func main() {
 	http.HandleFunc("/", handler)
 
-	// start HTTPS server in goroutine
+	//start HTTPS server in goroutine
 	go http.ListenAndServeTLS(":8081", "ssl/cert.pem", "ssl/key.pem", nil)
 
-	// start HTTP server old-school and redirect to HTTPS
+	//start HTTP server old-school and redirect to HTTPS
 	http.ListenAndServe(":8080", nil)
 	//http.HandlerFunc(httpsRedirect)
 }
