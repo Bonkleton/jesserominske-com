04685b20daf2b77a2cc2ff357d72983441712c2d
Jesse Rominske <sidorak12814@gmail.com>
2017-04-30 21:05:54 +0000

    added Mux handlers to server, prepared for TLS to work someday

--- backend/server.go
+++ backend/server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	//	"github.com/kabukky/httpscerts"
 	"io/ioutil"
+	//	"log"
 	"net/http"
 	"text/template"
 )
@@ -30,54 +32,77 @@ func loadPartials() (map[string]string, error) {
 	return g, nil
 }
 
-//HTTP request handler
-func handler(wrt http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.URL.Path)
-	if req.URL.Path == "/" { //load the index
-		//grab all partials
-		partials, err := loadPartials()
-		if err != nil {
-			http.Error(wrt, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		//get template function based on index and execute to load page
-		t, _ := template.ParseFiles("../index.html")
-		t.Execute(wrt, partials)
-	} else if req.URL.Path == "/favicon.ico" { //load favicon.ico because who cares
-		f, err := ioutil.ReadFile("../favicon.ico")
-		if err != nil {
-			http.Error(wrt, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprintf(wrt, "%s", f)
-	} else if req.URL.Path[:11] == "/css/images" { //if we want to load an image
-		f, err := ioutil.ReadFile("../" + req.URL.Path)
-		if err != nil {
-			http.Error(wrt, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprintf(wrt, "%s", f)
-	} else { //in all other cases load the index
-		//grab all partials
-		partials, err := loadPartials()
-		if err != nil {
-			http.Error(wrt, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		//get template function based on index and execute to load page
-		t, _ := template.ParseFiles("../index.html")
-		t.Execute(wrt, partials)
+//request handler for index
+func indexHandler(wrt http.ResponseWriter, req *http.Request) {
+	fmt.Println("website index")
+	//grab all partials
+	partials, err := loadPartials()
+	if err != nil {
+		http.Error(wrt, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	//get template function based on index and execute to load page
+	t, _ := template.ParseFiles("../index.html")
+	t.Execute(wrt, partials)
 }
 
-//start the servers
+//request handler for favicon because who cares
+func faviconHandler(wrt http.ResponseWriter, req *http.Request) {
+	fmt.Println("favicon")
+	f, err := ioutil.ReadFile("../favicon.ico")
+	if err != nil {
+		http.Error(wrt, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(wrt, "%s", f)
+}
+
+//request handler for images
+func imageHandler(wrt http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL.Path[12:])
+	f, err := ioutil.ReadFile("../" + req.URL.Path)
+	if err != nil {
+		http.Error(wrt, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(wrt, "%s", f)
+}
+
+//redirect HTTP to HTTPS (works, but unused)
+//func redirectToHttps(wrt http.ResponseWriter, req *http.Request) {
+//http.Redirect(wrt, req, "https://jesserominske-com-bonkleton.c9users.io:8081"+req.RequestURI, http.StatusMovedPermanently)
+//}
+
+//start the server
 func main() {
-	http.HandleFunc("/", handler)
+	//TODO: actually get TLS working, you idiot
+
+	//check if the cert files are available
+	//err := httpscerts.Check("../ssl/cert.pem", "../ssl/key.pem")
+	//if they are not available, generate new ones
+	//if err != nil {
+	//	err = httpscerts.Generate("../ssl/cert.pem", "../ssl/key.pem", "https://jesserominske-com-bonkleton.c9users.io:8081")
+	//	if err != nil {
+	//		log.Fatal("Error: Couldn't create https certs.")
+	//	}
+	//}
+
+	//create a new ServeMux for HTTP connections
+	httpMux := http.NewServeMux()
+	httpMux.Handle("/", http.HandlerFunc(indexHandler))
+	httpMux.Handle("/favicon.ico", http.HandlerFunc(faviconHandler))
+	httpMux.Handle("/css/images/", http.HandlerFunc(imageHandler))
+
+	//create a new ServeMux for HTTPS connections
+	httpsMux := http.NewServeMux()
+	httpsMux.Handle("/", http.HandlerFunc(indexHandler))
+	httpsMux.Handle("/favicon.ico", http.HandlerFunc(faviconHandler))
+	httpsMux.Handle("/css/images/", http.HandlerFunc(imageHandler))
 
 	//start HTTPS server in goroutine
-	go http.ListenAndServeTLS(":8081", "ssl/cert.pem", "ssl/key.pem", nil)
+	go http.ListenAndServeTLS(":8081", "../ssl/cert.pem", "../ssl/key.pem", httpsMux)
 
 	//start HTTP server old-school and redirect to HTTPS
-	http.ListenAndServe(":8080", nil)
-	//http.HandlerFunc(httpsRedirect)
+	http.ListenAndServe(":8080", httpMux)
+	//http.ListenAndServe(":8080", http.HandlerFunc(redirectToHttps))
 }
