809898ae742995b1e676e7868792f2909b1f3a1c
Jesse Rominske <sidorak12814@gmail.com>
2017-05-10 00:49:49 -0400

    fixed bug regarding context

--- js/content.js
+++ js/content.js
@@ -3,26 +3,22 @@ class content {
   constructor() {
     this.model = new model(this);
   }
-  //function to swtich view in general
-  switchView(view, context) {
-    //change into appropriate page
-    $("#content").html($("#" + view).html());
-    //initialize proper controllers if necessary
-    if (!context.controller) {
-      if (view === "blogView") {
-        context.controller = new blogController(context).init();
-      }
-    }
-  }
   init() {
-    //magical incantation
-    var that = this;
     //initialize homepage on first load
     $("#content").html($("#aboutView").html());
+    //magical incantation
+    var that = this;
     //handler for navbar to switch view based on name
     $("#nav span").on("click", function() {
+      //change into appropriate page
       var view = $(this).attr("name") + "View";
-      that.switchView(view, that);
+      $("#content").html($("#" + view).html());
+      //initialize proper controllers if necessary
+      if (!that.controller) {
+        if (view === "blogView") {
+          that.controller = new blogController(that).init();
+        }
+      }
       //TODO: Add jQuery animations
     });
   }
